d305b0b7d82b4f7b0a1c459faede14b5ac5579ec
hyh <2667004988@qq.com>
2024-12-24 13:50:19 +0800

    更新

--- shell/ubuntu2204系统安装脚本.cmd
+++ shell/ubuntu2204系统安装脚本.cmd
@@ -1,6 +1,6 @@
 
 
-cmd /c start https://mirrors.ustc.edu.cn/ubuntu-releases/20.04.6/ubuntu-20.04.6-desktop-amd64.iso
+cmd /c start https://mirrors.ustc.edu.cn/ubuntu-releases/22.04.5/ubuntu-22.04.5-desktop-amd64.iso
 
 
 cmd /c start https://mirrors.sdu.edu.cn/github-release/1734114772/github-release/ventoy_Ventoy/v1.0.99/ventoy-1.0.99-windows.zip
